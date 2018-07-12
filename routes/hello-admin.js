3bcee44b58647510b937a3cd6e52214d01a907ed
BB-8 <ichannel.jakpat@gmail.com>
2018-07-12 13:25:09 +0700

    Remove unused routes

--- routes/hello-admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (adminExpressApp) {
-  adminExpressApp.get('/hello', (req, res) => {
-    res.json({hello: 'admin'});
-  });
-};
--- routes/hello-eg.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (gatewayExpressApp) {
-  gatewayExpressApp.get('/hello', (req, res) => {
-    res.json({hello: 'Express-Gateway'});
-  });
-};
