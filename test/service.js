71ef5e20b4c633b2faa912cb390286a497730a66
BB-8 <ichannel.jakpat@gmail.com>
2018-07-18 00:10:29 +0700

    Setup mock server

--- test/service.js
+++ test/service.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.get('', (req, res, next) => {
+app.get('*', (req, res, next) => {
+	console.log(req.headers)
+	res.json(req.headers)
+})
 
+app.listen(8081, () => {
+    console.log(`App is listening on PORT 8001`);
 })
 
 module.exports = app
