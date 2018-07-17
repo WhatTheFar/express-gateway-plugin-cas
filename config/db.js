d69e8e5c2131a3ae7e8ed23dd1263e6175e87c1c
BB-8 <ichannel.jakpat@gmail.com>
2018-07-17 22:35:31 +0700

    Support ENV variable for db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 
 
 // const uri = 'postgres://user:pass@example.com:5432/dbname'
-const uri = 'postgres://postgres:password@localhost:5432/postgres'
+const uri = process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/postgres'
 const sequelize = new Sequelize(uri)
 
 module.exports = sequelize
