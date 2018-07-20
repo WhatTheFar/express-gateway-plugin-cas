25a2332358e05fb63ea196eb2a75fed048167f8a
BB-8 <ichannel.jakpat@gmail.com>
2018-07-20 13:49:37 +0700

    Added delete user route

--- lib/routes/auth-user-eg.ts
+++ lib/routes/auth-user-eg.ts
@@ -2,6 +2,7 @@ import { Application } from 'express-serve-static-core';
 import User from '../models/user-model';
 import { jsonMiddleware } from './../config/middlewares';
 import { asyncifyHandler } from './../utils/async-handler';
+import { ResponseUtil } from './../utils/response-util';
 
 export default (gatewayExpressApp: Application) => {
 	gatewayExpressApp.post(
@@ -16,6 +17,17 @@ export default (gatewayExpressApp: Application) => {
 			} catch (error) {
 				next(error);
 			}
+		})
+	);
+
+	gatewayExpressApp.delete(
+		'/auth/user/:id',
+		asyncifyHandler(async (req, res, next) => {
+			const row = await User.destroy({ where: { username: req.params.id } });
+			if (row > 0) {
+				return res.send('Success');
+			}
+			return ResponseUtil.sendInvalidId(res);
 		})
 	);
 };
